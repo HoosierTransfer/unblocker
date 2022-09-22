75b723653ed98e988d4725ddce6bfd46a0a89c52
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-09-22 10:22:57 -0400

    fix

--- static/scripts/supabase.js
+++ static/scripts/supabase.js
@@ -15,14 +15,14 @@ async function signInWithGithub() {
 async function Signup() {
   document.cookie = `key=${encodeURIComponent(document.getElementById("key").value)} path=/; secure`
   document.cookie = `UUID=${encodeURIComponent(new DeviceUUID().get())} path=/; secure`
-  const { user, session, error } = await supabase.auth.signUp({
-    email: document.getElementById("mail").value,
-    password: document.getElementById("passwd").value
-  },
-  {
-    data: {
-      secretKey: document.getElementById("key").value,
-      deviceId: new DeviceUUID().get()
-    }
-  })
+  // const { user, session, error } = await supabase.auth.signUp({
+  //   email: document.getElementById("mail").value,
+  //   password: document.getElementById("passwd").value
+  // },
+  // {
+  //   data: {
+  //     secretKey: document.getElementById("key").value,
+  //     deviceId: new DeviceUUID().get()
+  //   }
+  // })
 }
