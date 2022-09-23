1a6edc7979a4b33ec355fd5280aedc20a0926c27
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-09-23 13:32:46 -0400

    fix

--- static/base.js
+++ static/base.js
@@ -24,3 +24,8 @@
 function signup() {
   alert("etest");
 }
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://hxyegpdslremfvirwunq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE');
