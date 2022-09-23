a8d29f313c07130482fa29dacac363d7ee787160
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-09-23 13:51:13 -0400

    fix

--- static/base.js
+++ static/base.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 var supabase;
+//supabase = createClient('https://hxyegpdslremfvirwunq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE');
 // async function logon() {
 //   const { user, session, error } = await supabase.auth.signIn({
 //     email: document.getElementById("mail").value,
@@ -24,4 +25,5 @@ async function init() {
 
 async function signUp() {
   const { user, session, error } = await supabase.auth.signUp({email: document.getElementById("mail").value,password: document.getElementById("passwd").value,});
+  alert(error);
 }
