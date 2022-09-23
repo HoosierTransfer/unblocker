6809d35876a26bca1651312faa545e82506a540b
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-09-23 13:54:09 -0400

    fix

--- static/base.js
+++ static/base.js
@@ -19,11 +19,12 @@ function signup() {
   alert("etest");
 }
 
+async function signUp() {
+  //const { user, session, error } = await supabase.auth.signUp({email: document.getElementById("mail").value,password: document.getElementById("passwd").value,});
+  alert("123");
+}
+
 async function init() {
   supabase = createClient('https://hxyegpdslremfvirwunq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE');
 }
 
-async function signUp() {
-  const { user, session, error } = await supabase.auth.signUp({email: document.getElementById("mail").value,password: document.getElementById("passwd").value,});
-  alert(error);
-}
