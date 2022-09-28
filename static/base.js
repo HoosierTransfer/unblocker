2e6aab2d61a96b0f13832ea7ee33c30b7584b89d
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-09-28 12:36:42 -0400

    ffff

--- static/base.js
+++ static/base.js
@@ -8,9 +8,9 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE'
 )
 
-const domContainer = document.querySelector('app');
+const domContainer = document.querySelector('#app');
 const root = ReactDOM.createRoot(domContainer);
 
-const App = () => <Auth supabaseClient={supabase} />
+const App = () => <Auth supabaseClient={supabase} />;
 
-root.render(App);
+root.render(<Auth supabaseClient={supabase} />);
