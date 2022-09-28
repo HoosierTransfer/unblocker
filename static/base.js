5c969dda8522088ca048d53191873298a1c43c72
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-09-28 11:52:16 -0400

    fix

--- static/base.js
+++ static/base.js
@@ -1,9 +1,25 @@
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient('https://hxyegpdslremfvirwunq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE');
+// async function logon() {
+//   const { user, session, error } = await supabase.auth.signIn({
+//     email: document.getElementById("mail").value,
+//     password: document.getElementById("passwd").value,
+//   });
+//   alert(error);
+// }
 
-const supabase = createClient(
-  'https://hxyegpdslremfvirwunq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE'
-)
+// async function signInWithGithub() {
+//   const { user, session, error } = await supabase.auth.signIn({
+//     provider: 'github',
+//   })
+// }
 
-const App = () => <Auth supabaseClient={supabase} />
+async function signUp() {
+  //const { user, session, error } = await supabase.auth.signUp({email: document.getElementById("mail").value,password: document.getElementById("passwd").value,});
+  alert("123");
+}
+
+// async function init() {
+//   supabase = createClient('https://hxyegpdslremfvirwunq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4eWVncGRzbHJlbWZ2aXJ3dW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM3NzM0NjEsImV4cCI6MTk3OTM0OTQ2MX0.h0EMF5FCpam2-IpzANEozOv1WOQXzGNwI32QyG1ELjE');
+// }
+
