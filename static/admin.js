b99d98eb4ac70faa418759b021344540d96b01ce
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-10-28 12:51:49 -0400

    fix

--- static/admin.js
+++ static/admin.js
@@ -1,11 +1,2 @@
-async function connectToServer() {
-    const ws = new WebSocket('ws://sciencehelp.herokuapp.com:7071/ws');
-    return new Promise((resolve, reject) => {
-        const timer = setInterval(() => {
-            if(ws.readyState === 1) {
-                clearInterval(timer)
-                resolve(ws);
-            }
-        }, 10);
-    });
-}
+const ws = new WebSocket('ws://sciencehelp.herokuapp.com:7071/ws');
+
