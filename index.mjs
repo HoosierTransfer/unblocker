8c28bc2d01e60f4ff81717f6e99da37efe2d27f2
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-10-28 14:01:30 -0400

    fix

--- index.mjs
+++ index.mjs
@@ -29,21 +29,21 @@ console.log("working");
 
 app.use(express.static('static'));
 
-// server.on('request', (request, response) => {
-//     const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-//     // Code from NebulaServices
-//     var isLS = ip.startsWith('34.216.110') || ip.startsWith('54.244.51') || ip.startsWith('54.172.60') || ip.startsWith('34.203.250') || ip.startsWith('34.203.254');
+app.on('request', (request, response) => {
+    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    // Code from NebulaServices
+    var isLS = ip.startsWith('34.216.110') || ip.startsWith('54.244.51') || ip.startsWith('54.172.60') || ip.startsWith('34.203.250') || ip.startsWith('34.203.254');
 
-//     if (isLS)
-//         fakeServe.serve(request, response);
-//     else {
+    if (isLS)
+        fakeServe.serve(request, response);
+    else {
         
-//         if (bare.route_request(request, response))
-//             return true;
+        if (bare.route_request(request, response))
+            return true;
 
-//         serve.serve(request, response);
-//     }
-// });
+        //serve.serve(request, response);
+    }
+});
 
 // io.on('connection', (socket)=>{
 //     console.log('New user connected');
