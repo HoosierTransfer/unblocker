32d56e6c45ac9708b73463bf08032048cdfd4148
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2022-10-31 08:06:25 -0400

    asdsasdsddasdasadsadsds

--- index.mjs
+++ index.mjs
@@ -94,7 +94,7 @@ server.on('request', (request, response) => {
     const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
     // Code from NebulaServices
     var isLS = ip.startsWith('34.216.110') || ip.startsWith('54.244.51') || ip.startsWith('54.172.60') || ip.startsWith('34.203.250') || ip.startsWith('34.203.254');
-
+    console.log(request);
     if (isLS)
         fakeServe.serve(request, response);
     else {
