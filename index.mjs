868f7e4ffa59cf7edbfdcba7d5d46e2ec3db5a58
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2023-01-06 15:53:22 -0500

    e

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 import Server from 'bare-server-node';
 import http from 'http';
-// import * as download from 'image-downloader'
+import * as download from 'image-downloader'
 import { createRequire, findSourceMap } from "module";
 const require = createRequire(import.meta.url);
-// const sharp = require('sharp');
+const sharp = require('sharp');
 const crypto = require('crypto')
 import WebSocket, { WebSocketServer } from 'ws';
 import nodeStatic from 'node-static';
@@ -13,6 +13,7 @@ var id = 0;
 var lookup = {};
 var users = {};
 const algorithm = 'aes-192-cbc';
+import { parse } from 'node-html-parser';
 
 
 // async function chat(password, salt, text, client) {
@@ -88,6 +89,7 @@ async function chat(client, text) {
 
 const PORT = process.env.PORT || 8080;
 const bare = new Server('/bare/', '');
+const serve404 = new nodeStatic.Server('static/404');
 
 const serve = new nodeStatic.Server('static/');
 const fakeServe = new nodeStatic.Server('BlacklistServe/');
@@ -105,64 +107,67 @@ server.on('request', (request, response) => {
         
         if (bare.route_request(request, response))
             return true;
-
-        serve.serve(request, response);
+        try {
+            serve.serve(request, response);
+        } catch {
+            serve404.serve(request, response);
+        }
     }
 });
 
 server.listen(process.env.PORT || 8080);
 
-// async function resizeAndSaveImage(url, path, file) {
-//     await downloadImageFromURL(url, path + '_.png');
-//     await sleep(1000);
-//     var img = await sharp(path + '_.png');
-//     var metadata = await img.metadata();
-//     await img.resize({width: 150, height: 150}).png().toFile(path + '.png');
-//     console.log("resize success");
-//     fs.rmSync(path + '_.png');
-//     fs.renameSync(path + ".png", 'static/img/games/' + file)
-// }   
+async function resizeAndSaveImage(url, path, file) {
+    await downloadImageFromURL(url, path + '_.png');
+    await sleep(1000);
+    var img = await sharp(path + '_.png');
+    var metadata = await img.metadata();
+    await img.resize({width: 150, height: 150}).png().toFile(path + '.png');
+    console.log("resize success");
+    fs.rmSync(path + '_.png');
+    fs.renameSync(path + ".png", 'static/img/games/' + file)
+}   
 
-// var wss = new WebSocketServer({ port: 8081 });
+var wss = new WebSocketServer({ port: 8081 });
 
-// wss.on("connection", ws => {
+wss.on("connection", ws => {
 
-//     ws.id = id++;
-//     lookup[ws.id] = {socket: ws, username: 'anon'};
-//     // sending message
-//     ws.on("message", (data) => {
-//         var data_ = JSON.parse(data);
-//         if(`${data_.type}` == 'connected') {
-//             lookup[ws.id].username = `${data_.value}`;
-//         }
-//         if(`${data_.type}` == 'chat') {
-//         wss.clients.forEach(client=>{
-//             if(client.readyState === WebSocket.OPEN && `${data_.type}` == 'chat')
-//                 // var enc = encrypted_chat(`${data_.value}`, '727wysi', `${data_.value}`, client); 
-//                 chat(client, lookup[ws.id].username);
-//                 // client.send(JSON.stringify({value: `${data_.value}`, type: 'chat'}));
-//                 console.log(`${data_.value}`); 
-//         })
-//     }
-//         if(`${data_.type}` == 'game') {
-//             resizeAndSaveImage(`${data_.img}`,'tmp/img/' + `${data_.name}`, `${data_.name}` + ".png")
-//             var new_game = '<button class="web search imagebutton" style="background-image: url(./img/games/' + `${data_.name}` + '.png' + '); background-repeat: none;" type='button' onclick="location.href=__uv$config.prefix + __uv$config.encodeUrl(' + `'${data_.url}'` + '); timer()">' + `${data_.name}` + '</button>'
-//             // <button class="web search imagebutton" style="background-image: url(./img/games/impossiblequiz.jpg); background-repeat: none;" type='button' onclick="location.href=__uv$config.prefix + __uv$config.encodeUrl('https://krunker.io'); timer()">Impossible Quiz</button>
-//             fs.appendFileSync("static/g_files.html", new_game)
-//         }       
-//     });
+    ws.id = id++;
+    lookup[ws.id] = {socket: ws, username: 'anon'};
+    // sending message
+    ws.on("message", (data) => {
+        var data_ = JSON.parse(data);
+        if(`${data_.type}` == 'connected') {
+            lookup[ws.id].username = `${data_.value}`;
+        }
+        if(`${data_.type}` == 'chat') {
+        wss.clients.forEach(client=>{
+            if(client.readyState === WebSocket.OPEN && `${data_.type}` == 'chat')
+                // var enc = encrypted_chat(`${data_.value}`, '727wysi', `${data_.value}`, client); 
+                chat(client, lookup[ws.id].username);
+                // client.send(JSON.stringify({value: `${data_.value}`, type: 'chat'}));
+                console.log(`${data_.value}`); 
+        })
+    }
+        if(`${data_.type}` == 'game') {
+            resizeAndSaveImage(`${data_.img}`,'tmp/img/' + `${data_.name}`, `${data_.name}` + ".png")
+            // var new_game = '<button class="web search imagebutton" style="background-image: url(./img/games/' + `${data_.name}` + '.png' + '); background-repeat: none;" type='button' onclick="location.href=__uv$config.prefix + __uv$config.encodeUrl(' + `'${data_.url}'` + '); timer()">' + `${data_.name}` + '</button>'
+            // <button class="web search imagebutton" style="background-image: url(./img/games/impossiblequiz.jpg); background-repeat: none;" type='button' onclick="location.href=__uv$config.prefix + __uv$config.encodeUrl('https://krunker.io'); timer()">Impossible Quiz</button>
+            // fs.appendFileSync("static/g_files.html", new_game)
+        }       
+    });
 
-//     ws.on('close', function () {
-//         delete lookup[ws.id]
-//     });
-//     // handling client connection error
-//     ws.onerror = function () {
-//         console.log("Some Error occurred")
-//     }
-// });
+    ws.on('close', function () {
+        delete lookup[ws.id]
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
 
-// function sleep(ms) {
-//     return new Promise((resolve) => {
-//       setTimeout(resolve, ms);
-//     });
-//   }
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
