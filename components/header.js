792a24e919d5c2251d111aaf7bb382b9499c4881
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2023-01-24 17:18:24 -0500

    d

--- components/header.js
+++ components/header.js
@@ -5,6 +5,8 @@ function header() {
         <div>
         <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0"></script>
         <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro-v6@44659d9/css/all.min.css" rel="stylesheet" type="text/css" />
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1212928658818708"
+     crossorigin="anonymous"></script>
         <link rel="stylesheet" href="./new_css.css" />
         <title>DEV</title>
         </div>
